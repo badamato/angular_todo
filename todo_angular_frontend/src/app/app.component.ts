df03e1ee6321c3675c0d19fa3c456d4c09ad75a8
badamato <bethadamato@gmail.com>
2025-05-05 23:19:18 -0400

    finalizes the CRUD elements

--- todo_angular_frontend/src/app/app.component.ts
+++ todo_angular_frontend/src/app/app.component.ts
@@ -1,46 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   todos: Array<any> = []
 
   baseUrl: string = "http://localhost:3000/todos"
 
-  //properties to bind with create form
+  //Properties to Bind with Create Form
   createTitle: string = ""
   createBody: string = ""
 
-  //function to get list of todos
+  //Properties to Bind with Create Form
+  editTitle: string = ""
+  editBody: string = ""
+  editId: number = 0
+
+  //Function to Grab list of todos
   async getTodos() {
     const response = await fetch(this.baseUrl)
     const data = await response.json()
     this.todos = await data
   }
 
-  //takes the date from the form and creates a new todo
+  //takes data from form and creates new todo
   async createTodo() {
-    console.log(this.createTitle, this.createBody)
     await fetch(this.baseUrl, {
-      method: "POST",
+      method: "post",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ 
+      body: JSON.stringify({
         title: this.createTitle,
-        body: this.createBody
+        body: this.createBody,
       }),
-  })
-  //updates todo list and resets form
-  this.getTodos()
-  this.createTitle = ""
-  this.createBody = ""
+    })
+    //update todo list and reset form
+    this.getTodos()
+    this.createTitle = ""
+    this.createBody = ""
   }
 
-  //function runs when the component loads
+  editSelect(todo) {
+    this.editId = todo.id
+    this.editTitle = todo.title
+    this.editBody = todo.body
+  }
+
+  //takes data from form and updates new todo
+  async updateTodo() {
+    await fetch(this.baseUrl + "/" + this.editId, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: this.editTitle,
+        body: this.editBody,
+      }),
+    })
+    //update todo list and reset form
+    this.getTodos()
+    this.editTitle = ""
+    this.editBody = ""
+    this.editId = 0
+  }
+
+
+  //takes data from form and deletes todo
+  async deleteTodo(todo) {
+    await fetch(this.baseUrl + "/" + todo.id, {
+      method: "delete",
+    })
+    //update list of todos
+    this.getTodos()
+  }
+
+
+
+  //this function runs when the component loads
   ngOnInit() {
     this.getTodos()
   }
